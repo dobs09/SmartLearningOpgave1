8410b8a76d466a79d7f8d6256aaf6e22e656da04
Tobias Høj-Kristensen <Tobias.Hoj-Kristensen@egekom.dk>
2021-03-19 15:41:41 +0100

    Add project files.

---
+++ SmartLearningOpgave1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SmartLearningOpgave1.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartLearningOpgave1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Signup(User user)
+        {
+            return View(user);
+        }
+
+        public IActionResult UserDetails(User user)
+        {
+            return View(user);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SmartLearningOpgave1/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartLearningOpgave1.Models
+{
+    public class User
+    {
+        [Required]
+        public string firstName { get; set; }
+        [Required]
+        public string lastName { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Please enter a valid email")]
+        public string email { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Please enter a valid email")]
+        [Compare("email", ErrorMessage = "Email address does not match")]
+        public string confirmEmail { get; set; }
+
+        [Required]
+        public string city { get; set; }
+
+        [Required]
+        public string country { get; set; }
+
+    }
+}
